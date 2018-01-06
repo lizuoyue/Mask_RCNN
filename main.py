ceca803a53464114fecdd5f9079821d698f1029d
lizuoyue <li.zuoyue@foxmail.com>
2018-01-06 14:01:21 +0100

    a

---
+++ main.py
@@ -0,0 +1,290 @@
+import os
+import sys
+import random
+import math
+import re
+import time
+import numpy as np
+import cv2
+import matplotlib
+import matplotlib.pyplot as plt
+import paramiko, io
+from PIL import Image, ImageDraw
+
+from config import Config
+import utils
+import model as modellib
+import visualize
+from model import log
+
+# Root directory of the project
+ROOT_DIR = os.getcwd()
+
+# Directory to save logs and trained model
+MODEL_DIR = os.path.join(ROOT_DIR, "logs")
+
+# Local path to trained weights file
+COCO_MODEL_PATH = os.path.join(ROOT_DIR, "mask_rcnn_coco.h5")
+# Download COCO trained weights from Releases if needed
+if not os.path.exists(COCO_MODEL_PATH):
+    utils.download_trained_weights(COCO_MODEL_PATH)
+
+class ShapesConfig(Config):
+    """Configuration for training on the toy shapes dataset.
+    Derives from the base Config class and overrides values specific
+    to the toy shapes dataset.
+    """
+    # Give the configuration a recognizable name
+    NAME = "shapes"
+
+    # Train on 1 GPU and 8 images per GPU. We can put multiple images on each
+    # GPU because the images are small. Batch size is 8 (GPUs * images/GPU).
+    GPU_COUNT = 1
+    IMAGES_PER_GPU = 8
+
+    # Number of classes (including background)
+    NUM_CLASSES = 1 + 1  # background + 3 shapes
+
+    # Use small images for faster training. Set the limits of the small side
+    # the large side, and that determines the image shape.
+    IMAGE_MIN_DIM = 640
+    IMAGE_MAX_DIM = 640
+
+    # Use smaller anchors because our image and objects are small
+    RPN_ANCHOR_SCALES = (80, 160, 240, 320)  # anchor side in pixels
+
+    # Reduce training ROIs per image because the images are small and have
+    # few objects. Aim to allow ROI sampling to pick 33% positive ROIs.
+    TRAIN_ROIS_PER_IMAGE = 32
+
+    # Use a small epoch since the data is simple
+    STEPS_PER_EPOCH = 100
+
+    # use small validation steps since the epoch is small
+    VALIDATION_STEPS = 5
+    
+config = ShapesConfig()
+config.display()
+
+def get_ax(rows=1, cols=1, size=8):
+    """Return a Matplotlib Axes array to be used in
+    all visualizations in the notebook. Provide a
+    central point to control graph sizes.
+    
+    Change the default size attribute to control the size
+    of rendered images
+    """
+    _, ax = plt.subplots(rows, cols, figsize=(size*cols, size*rows))
+    return ax
+
+class ShapesDataset(utils.Dataset):
+    """Generates the shapes synthetic dataset. The dataset consists of simple
+    shapes (triangles, squares, circles) placed randomly on a blank surface.
+    The images are generated on the fly. No file access required.
+    """
+
+    def load_images(self, flag, data_path):
+        """Generate the requested number of synthetic images.
+        count: number of images to generate.
+        height, width: the size of the generated images.
+        """
+        # Add classes
+        self.add_class("types", 1, "house")
+
+        # Add images
+        # Generate random specifications of images (i.e. color and
+        # list of shapes sizes and locations). This is more compact than
+        # actual images. Images are generated on the fly in load_image().
+        self.lzy_ssh = paramiko.SSHClient()
+        self.lzy_ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        self.lzy_ssh.connect('cab-e81-28.ethz.ch', username = 'zoli', password = '64206960lzyLZY')
+        self.lzy_sftp = self.lzy_ssh.open_sftp()
+
+        with open('./AreaIdxList.txt', 'r') as f:
+            self.lzy_area_idx_list = eval(f.read())
+        print('Totally %d areas.' % len(self.lzy_area_idx_list))
+
+        #
+        train_prob = 0.9
+        split = int(train_prob * len(self.lzy_area_idx_list))
+        if flag:
+            self.lzy_idx_list = self.lzy_area_idx_list[:split]
+        else:
+            self.lzy_idx_list = self.lzy_area_idx_list[split:]
+
+        for i in range(len(self.lzy_idx_list)):
+            self.add_image("types", image_id=i, path=data_path+"/"+self.lzy_idx_list[i])
+
+    def load_image(self, image_id):
+        """Generate an image from the specs of the given image ID.
+        Typically this function loads the image from a file, but
+        in this case it generates the image on the fly from the
+        specs in image_info.
+        """
+        info = self.image_info[image_id]
+        img = Image.open(io.BytesIO(self.lzy_sftp.open(info['path'] + '/img.png').read()))
+        return np.array(img).astype(np.uint8)
+
+    def image_reference(self, image_id):
+        """Return the shapes data of the image."""
+        info = self.image_info[image_id]
+        if info["source"] == "type":
+            return info["type"]
+        else:
+            super(self.__class__).image_reference(self, image_id)
+
+    def load_mask(self, image_id):
+        """Generate instance masks for shapes of the given image ID.
+        """
+        info = self.image_info[image_id]
+        polygons = []
+        lines = self.lzy_sftp.open(info['path'] + '/polygons.txt').read().decode('utf-8').split('\n')
+        for line in lines:
+            if line.strip() != '':
+                if line.strip() == '%':
+                    polygons.append([])
+                else:
+                    x, y = line.strip().split()
+                    x, y = int(x), int(y)
+                    polygons[-1].append((x, y))
+
+        temp = [[Image.new('RGB', (640, 640), color = 0), True] for _ in range(len(polygons))]
+        for i, polygon in enumerate(polygons):
+            draw = ImageDraw.Draw(temp[i][0])
+            draw.polygon(polygon, fill = 255, outline = 255)
+            if np.array(temp[i][0]).astype(np.int32).sum() == 0:
+                temp[i][1] = False
+        temp = [item[1] for item in temp if item[0]]
+
+        mask = np.zeros([640, 640, len(temp)], dtype=np.uint8)
+        for i, item in enumerate(temp):
+            mask[..., i] = np.array(item)
+
+        class_ids = np.array([self.class_names.index('house') for _ in temp])
+        return mask, class_ids.astype(np.int32)
+
+# Training dataset
+dataset_train = ShapesDataset()
+dataset_train.load_images(True, '/local/lizuoyue/Chicago_Area')
+dataset_train.prepare()
+
+# Validation dataset
+dataset_val = ShapesDataset()
+dataset_val.load_images(False, '/local/lizuoyue/Chicago_Area')
+dataset_val.prepare()
+
+# Create model in training mode
+model = modellib.MaskRCNN(mode="training", config=config,
+                          model_dir=MODEL_DIR)
+
+# Which weights to start with?
+init_with = "coco"  # imagenet, coco, or last
+
+if init_with == "imagenet":
+    model.load_weights(model.get_imagenet_weights(), by_name=True)
+elif init_with == "coco":
+    # Load weights trained on MS COCO, but skip layers that
+    # are different due to the different number of classes
+    # See README for instructions to download the COCO weights
+    model.load_weights(COCO_MODEL_PATH, by_name=True,
+                       exclude=["mrcnn_class_logits", "mrcnn_bbox_fc", 
+                                "mrcnn_bbox", "mrcnn_mask"])
+elif init_with == "last":
+    # Load the last model you trained and continue training
+    model.load_weights(model.find_last()[1], by_name=True)
+
+# Train the head branches
+# Passing layers="heads" freezes all layers except the head
+# layers. You can also pass a regular expression to select
+# which layers to train by name pattern.
+model.train(dataset_train, dataset_val, 
+            learning_rate=config.LEARNING_RATE, 
+            epochs=1, 
+            layers='heads')
+
+# Fine tune all layers
+# Passing layers="all" trains all layers. You can also 
+# pass a regular expression to select which layers to
+# train by name pattern.
+model.train(dataset_train, dataset_val, 
+            learning_rate=config.LEARNING_RATE / 10,
+            epochs=2, 
+            layers="all")
+
+class InferenceConfig(ShapesConfig):
+    GPU_COUNT = 1
+    IMAGES_PER_GPU = 1
+
+inference_config = InferenceConfig()
+
+# Recreate the model in inference mode
+model = modellib.MaskRCNN(mode="inference", 
+                          config=inference_config,
+                          model_dir=MODEL_DIR)
+
+# Get path to saved weights
+# Either set a specific path or find last trained weights
+# model_path = os.path.join(ROOT_DIR, ".h5 file name here")
+model_path = model.find_last()[1]
+
+# Load trained weights (fill in path to trained weights here)
+assert model_path != "", "Provide path to trained weights"
+print("Loading weights from ", model_path)
+model.load_weights(model_path, by_name=True)
+
+
+# Test on a random image
+image_id = random.choice(dataset_val.image_ids)
+original_image, image_meta, gt_class_id, gt_bbox, gt_mask =\
+    modellib.load_image_gt(dataset_val, inference_config, 
+                           image_id, use_mini_mask=False)
+
+log("original_image", original_image)
+log("image_meta", image_meta)
+log("gt_class_id", gt_class_id)
+log("gt_bbox", gt_bbox)
+log("gt_mask", gt_mask)
+
+visualize.display_instances(original_image, gt_bbox, gt_mask, gt_class_id, 
+                            dataset_train.class_names, figsize=(8, 8))
+
+results = model.detect([original_image], verbose=1)
+
+r = results[0]
+visualize.display_instances(original_image, r['rois'], r['masks'], r['class_ids'], 
+                            dataset_val.class_names, r['scores'], ax=get_ax())
+
+# Compute VOC-Style mAP @ IoU=0.5
+# Running on 10 images. Increase for better accuracy.
+image_ids = np.random.choice(dataset_val.image_ids, 10)
+APs = []
+for image_id in image_ids:
+    # Load image and ground truth data
+    image, image_meta, gt_class_id, gt_bbox, gt_mask =\
+        modellib.load_image_gt(dataset_val, inference_config,
+                               image_id, use_mini_mask=False)
+    molded_images = np.expand_dims(modellib.mold_image(image, inference_config), 0)
+    # Run object detection
+    results = model.detect([image], verbose=0)
+    r = results[0]
+    # Compute AP
+    AP, precisions, recalls, overlaps =\
+        utils.compute_ap(gt_bbox, gt_class_id,
+                         r["rois"], r["class_ids"], r["scores"])
+    APs.append(AP)
+    
+print("mAP: ", np.mean(APs))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
